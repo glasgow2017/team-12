d9ebcff93945ea79d4c34a98f6eae541cd16544a
didi04 <2091499s@student.gla.ac.uk>
2017-11-04 19:54:14 +0000

    initialview and loginview layouts added

--- app/src/main/java/com/test/glasgowteam12/Activities/DialActivity.java
+++ app/src/main/java/com/test/glasgowteam12/Activities/DialActivity.java
@@ -3,6 +3,8 @@ package com.test.glasgowteam12.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.test.glasgowteam12.R;
+
 public class DialActivity extends AppCompatActivity {
 
     @Override
