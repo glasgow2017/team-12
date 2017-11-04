432246e5a2f4dfbb1ab56e046bc9e0f8e4c3c6e1
Chloe Collins <chloe.collins.contact@gmail.com>
2017-11-04 21:26:06 +0000

    Further PHP update #BestCodeEver #ThisAgain

--- php/login.php
+++ php/login.php
@@ -7,24 +7,29 @@ if(!$con){
 if (!mysqli_select_db ($con,'user_profiles')) {
  echo 'Database Not Selected';
 }
+$response = array();
+$code_f = "login_failed";
+$code_s = "login_success";
 
 session_start();
-// get JSON user login data from Android app
-$user_registration_info = file_get_contents("php://input");
-
-// Extract relevant info to check in DB
-$email = $user_registration_info["email"];
-$password = $user_registration_info["password"];
+// Extract relevant info from input to check in DB
+$email = $_POST["email"];
+$password = $_POST["password"];
 
 // Check user profile DB-if unique user profile present then retrieve associated data
 $sql_usercheck = mysqli_prepare($con, "SELECT userID FROM user_profile WHERE email='?' AND password='?");
 mysqli_stmt_bind_param($sql_usercheck,"ss", $email, $password);
 
-// Return unique user ID if user login info is correct
 if ($result = mysqli_query($link, $query)) {
-    while ($row = mysqli_fetch_row($result)) {
-        // Set userID as session variable
-        $_SESSION['userid'] = $row[0];
+    // Successful login
+    if (mysqli_num_rows($result) > 0){
+        $_SESSION['userid'] = $row[0]; // Set userID as session variable
+        array_push($response, array("code"=>$code_s));
+        echo json_encode($response);
+    // Unsuccessful login
+    } else {        
+        array_push($response, array("code"=>$code_f));
+        echo json_encode($response);
     }
     mysqli_free_result($result);
 }
