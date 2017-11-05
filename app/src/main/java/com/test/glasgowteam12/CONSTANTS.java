2934aa503e07bf093f478d03e3f42e82892ccdd4
Chloe Collins <chloe.collins.contact@gmail.com>
2017-11-05 12:02:52 +0000

    Last update

---
+++ app/src/main/java/com/test/glasgowteam12/CONSTANTS.java
@@ -0,0 +1,11 @@
+package com.test.glasgowteam12;
+
+/**
+ * Created by chloe on 05/11/17.
+ */
+
+public class CONSTANTS {
+
+    public static final String SnichSecret = "ddMHhvXpXEmHbuAEM8ZJvw==";
+    public static final String SnichKey = "9facc049-1e97-461c-9169-0f10ca66fcbe";
+}
