3b6934086ed97783eb4325cba8db8cba2b9e3eb0
Chloe Collins <chloe.collins.contact@gmail.com>
2017-11-05 05:36:48 +0000

    Re-add PHP files

--- php/login.php
+++ php/login.php
@@ -1,37 +1,37 @@
 <?php 
-$con = mysqli_connect('localhost','user','password', 'user_profiles');
-
+$config = require_once('config.php');
+$con = mysqli_connect($config['HOST'], $config['USERNAME'], $config['PASSWORD'], $config['DATABASE']);
 if(!$con){
  echo 'Not Connected To Server';
 }
-if (!mysqli_select_db ($con,'user_profiles')) {
+if (!mysqli_select_db ($con,$config['DATABASE'])) {
  echo 'Database Not Selected';
-}
+ }
 $response = array();
 $code_f = "login_failed";
 $code_s = "login_success";
 
 session_start();
 // Extract relevant info from input to check in DB
-$email = $_POST["email"];
-$password = $_POST["password"];
+$email = mysqli_escape_string($con, $_POST["email"]);
+$password = mysqli_escape_string($con, $_POST["password"]);
+echo $email . $password;
 
 // Check user profile DB-if unique user profile present then retrieve associated data
-$sql_usercheck = mysqli_prepare($con, "SELECT userID FROM user_profile WHERE email='?' AND password='?");
+$sql_usercheck = mysqli_prepare($con, "SELECT userID FROM user_profiles WHERE email='?' AND password='?");
 mysqli_stmt_bind_param($sql_usercheck,"ss", $email, $password);
+mysqli_stmt_execute($sql_usercheck) or die("Unable to execute query");
+mysqli_stmt_bind_result($sql_usercheck, $userID);
 
-if ($result = mysqli_query($link, $query)) {
-    // Successful login
-    if (mysqli_num_rows($result) > 0){
-        $_SESSION['userid'] = $row[0]; // Set userID as session variable
-        array_push($response, array("code"=>$code_s));
-        echo json_encode($response);
-    // Unsuccessful login
-    } else {        
-        array_push($response, array("code"=>$code_f));
-        echo json_encode($response);
-    }
-    mysqli_free_result($result);
+// Successful login
+if(mysqli_stmt_fetch($sql_usercheck)){
+  $_SESSION['user'] = $userID; // Set userID as session variable
+  array_push($response, array("code"=>$code_s));
+  echo json_encode($response);
+// Unsuccesful login
+} else {
+  array_push($response, array("code"=>$code_f));
+  echo json_encode($response);
 }
 mysqli_stmt_close($sql_usercheck);
 ?>
